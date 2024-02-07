717f6e6f85dd1cecf31d0d77bf221b3cde2c6e26
anir-ab <anibhagwat17@gmail.com>
2024-02-08 00:22:03 +0530

    pattern matching for urgency

--- extension/scripts/content.js
+++ extension/scripts/content.js
@@ -1,1 +1,17 @@
-console.log(document)
+var urgency = [/\d+\s+available/, /only\s+\d+\s+left/,/sale\s+ends\s+in+\d/,
+"hurry up", "ending soon", "last chance","limited time","limited offer","limited stock"];
+
+var a = document.documentElement.outerHTML;
+// console.log(a,"bad");
+console.log(urgency);
+function DPchecker(list){
+    for (var i = 0; i < list.length; i++) {
+        console.log("checking "+list[i]);
+        if (a.match(list[i])) {
+            alert("This page contains a Dark Pattern !!! " +list[i]);
+            break;
+        }
+    }
+}
+
+DPchecker(urgency);
