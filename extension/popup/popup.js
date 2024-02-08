0bb93ef7eafb725402d29295b7748cad2d4c7208
kr-2003 <kumar.kr.abhinav@gmail.com>
2024-02-08 23:46:23 +0530

    some changes

--- extension/popup/popup.js
+++ extension/popup/popup.js
@@ -1,6 +1,7 @@
+
 document.addEventListener('DOMContentLoaded', function () {
     document.getElementById('openNewTab').addEventListener('click', function () {
-        chrome.tabs.create({ url: 'chrome://newtab' }, function (tab) {
+        chrome.tabs.create({ url: 'chrome://randomTab' }, function (tab) {
             chrome.scripting.executeScript({
                 target: { tabId: tab.id },
                 function: function () {
@@ -9,4 +10,29 @@ document.addEventListener('DOMContentLoaded', function () {
             });
         });
     });
+
 });
+
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+    
+    if (request.cmd === "price_dipping") {
+        const div = document.createElement('div');
+        div.textContent = request.data.content;
+        document.body.appendChild(div);
+        console.log(request);
+    }
+});
+
+chrome.runtime.sendMessage({ message: "getElementInfo" }, function(response) {
+    var elementInfo = response.data;
+    if (elementInfo) {
+        console.log("Received element info from background.js:", elementInfo);
+        // Do something with the element info if needed
+    } else {
+        console.log("Element information not available.");
+    }
+});
+
+
+
+
--- extension/scripts/background.js
+++ extension/scripts/background.js
@@ -1,1 +1,35 @@
-console.log("hi");
+// Description: This script runs in the background and listens for messages from content.js
+
+var cart = new Set();
+
+
+
+function sendPopup(){
+  
+}
+
+
+
+// listen for message from content.js
+chrome.runtime.onMessage.addListener(
+  function(request, sender, sendResponse) {
+    console.log("message from content.js", request);
+    if (request.cmd === "add") {
+      cart.add(request);  
+    }
+    else if(request.cmd === "finalTotal") {
+      console.log("Total price:", request.totalPrice);
+      let calculatedTotalPrice = 0;
+      cart.forEach((item) => {
+        calculatedTotalPrice += item.price;
+      });
+      let totalPriceInInt = parseInt(request.totalPrice.replace(/[^0-9.-]+/g,""));
+      console.log("Calc Total price:", calculatedTotalPrice);
+
+      let differenceInPrice = Math.abs(calculatedTotalPrice - totalPriceInInt);
+      console.log(differenceInPrice);
+      chrome.runtime.sendMessage({ cmd: 'price_dipping', data: {subject: "Price Difference", content: differenceInPrice} });
+
+    }
+});
+
