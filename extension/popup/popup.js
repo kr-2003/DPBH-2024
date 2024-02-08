31ae8743f33b49d4228146cea1a9ed958bef2c29
anir-ab <anibhagwat17@gmail.com>
2024-02-09 01:14:49 +0530

    basic crowdsource fetching

--- extension/popup/popup.js
+++ extension/popup/popup.js
@@ -3,5 +3,28 @@ document.addEventListener('DOMContentLoaded', function () {
         const url = chrome.runtime.getURL('../newtab/newtab.html');
         chrome.tabs.create({ url: url });
     });
+    
+    const url = "http://127.0.0.1:8000";
+    const temp = "abcd";
+    var params = {
+        method:'POST',
+        headers: {
+            'Content-Type': 'application/json' 
+        },
+        body: JSON.stringify({ website: temp})
+    }
+    
+    var crowdsource;
+
+    fetch(url,params)
+        .then(response => response.json())
+        .then(data => {
+            crowdsource = data;
+            console.log(crowdsource);
+        })
+        .catch(error => console.error('Error:', error));
+    
+    document.getElementsByClassName('crowdsource-results')[0].innerHTML = "data from server: " + crowdsource;
+    
 });
 
