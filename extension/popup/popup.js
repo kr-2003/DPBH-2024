24d3602df0eddb5d206f4ca54490a73d7f55b9c6
Abhinav Kumar <kumar.kr.abhinav@gmail.com>
2024-02-10 02:28:16 +0530

    ui

--- extension/popup/popup.js
+++ extension/popup/popup.js
@@ -1,18 +1,9 @@
 document.addEventListener('DOMContentLoaded', function () {
-    document.getElementById('openNewTab').addEventListener('click', function () {
+    document.querySelector('.local-footer-button').addEventListener('click', function () {
         const url = chrome.runtime.getURL('../newtab/newtab.html');
         chrome.tabs.create({ url: url });
     });
     
-const url = "http://localhost:3000/index";
-const temp = "1 2 3 4 5 6 7 8 9 0";
-var params = {
-    method:'POST',
-    headers: {
-        'Content-Type': 'application/json' 
-    },
-    body: JSON.stringify({ website: temp})
-}
 
 
 fetch(url,params)
@@ -29,12 +20,44 @@ fetch(url,params)
 });
 
 
-var infoButton = document.getElementById('info');
-var resultPopup = document.getElementsByClassName('detailed-results')[0];
+// var infoButton = document.getElementById('info');
+// var resultPopup = document.getElementsByClassName('detailed-results')[0];
 
-function showPopup() { resultPopup.style.display = "block"; }
-function hidePopup() { resultPopup.style.display = "none"; }
+// function showPopup() { resultPopup.style.display = "block"; }
+// function hidePopup() { resultPopup.style.display = "none"; }
 
-infoButton.addEventListener('mouseover', showPopup);
-infoButton.addEventListener('mouseleave', hidePopup);
+// infoButton.addEventListener('mouseover', showPopup);
+// infoButton.addEventListener('mouseleave', hidePopup);
 
+// var button1 = document.querySelector('#button1');
+
+// button1.addEventListener('click', function() {
+//     alert("hello")
+//     changeContent('content1');
+// }
+// );
+
+var button1 = document.getElementById('button1');
+var button2 = document.getElementById('button2');
+
+button1.addEventListener('click', function() {
+    changeContent('local');
+});
+
+button2.addEventListener('click', function() {
+    changeContent('crowdsource');
+});
+
+function changeContent(contentId) {
+    var contentDiv = document.getElementById('content');
+    var button1 = document.getElementById('button1');
+    var button2 = document.getElementById('button2');
+    // Hide all buttons
+    button1.classList.remove('active');
+    button2.classList.remove('active');
+
+    document.getElementById(contentId === 'local' ? 'button1' : 'button2').classList.add('active');
+    document.getElementById(contentId === 'local' ? 'crowdsource' : 'local').classList.add('hidden-content');
+    document.getElementById(contentId === 'local' ? 'local' : 'crowdsource').classList.remove('hidden-content');
+  }
+  
